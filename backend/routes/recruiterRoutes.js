7cedc01ea99c2028b7cd74be07a03dbcd3e47c06
Piyush Pushkar <piyush.pushkar@stltech.in>
2025-03-02 16:22:12 +0530

    Update recruiterRoutes.js

--- backend/routes/recruiterRoutes.js
+++ backend/routes/recruiterRoutes.js
@@ -1,16 +1,29 @@
-// routes/recruiterRoutes.js
 const express = require('express');
 const router = express.Router();
 const recruiterController = require('../controllers/recruiterController');
 const authenticateToken = require('../middleware/recruiterMiddleware');
 
-// POST: Recruiter Sign-In (Send OTP)
-router.post('/signin', recruiterController.recruiterSignin);
+// POST: Recruiter Sign-In (OTP Send) - No authentication needed
+router.post('/signin', recruiterController.loginRecruiter);
 
-// POST: Verify OTP
-router.post('/verify-otp', recruiterController.verifyRecruiterOtp);
+// POST: Verify OTP - No authentication needed
+router.post('/verify-otp', recruiterController.verifyLoginOtp);
 
-// Example of a protected route (if needed later)
-// router.get('/protected-route', authenticateToken, recruiterController.someProtectedFunction);
+console.log('Controller Object:', recruiterController);
+
+// Temp job post routes 
+router.post('/jobs/draft', authenticateToken, recruiterController.createJobDraft);
+router.patch('/jobs/draft', authenticateToken, recruiterController.updateJobDraft);
+router.get('/jobs/draft/:sessionId', authenticateToken, recruiterController.getJobDraftPreview);
+router.post('/jobs/publish', authenticateToken, recruiterController.createJobFromDraft);
+router.delete('/jobs/draft/:sessionId', authenticateToken, recruiterController.deleteJobDraft);
+
+// Job Post routes 
+router.post('/jobs', authenticateToken, recruiterController.createJobPost);
+//router.get('/jobs', authenticateToken, recruiterController.getRecruiterJobs);
+router.get('/jobs', authenticateToken, recruiterController.getJobPosts);
+router.get('/jobs/:id', authenticateToken, recruiterController.getJobPostById);
+router.put('/jobs/:id', authenticateToken, recruiterController.updateJobPost);
+router.delete('/jobs/:id', authenticateToken, recruiterController.deleteJobPost);
 
 module.exports = router;
