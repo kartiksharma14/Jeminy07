c996c066883e18928bb42eb3d4ded469c8d7c4e9
Piyush Pushkar <piyush.pushkar@stltech.in>
2025-03-02 16:07:00 +0530

    Update job_post.sql

--- backend/database/job_post.sql
+++ backend/database/job_post.sql
@@ -1,33 +1,41 @@
 -- Use the database
-USE candidateprofile;
-CREATE TABLE job_post (
-    job_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    job_title VARCHAR(255),
-    location VARCHAR(255),
-    job_highlights VARCHAR(255),
-    employment_type VARCHAR(100),
-    key_skills TEXT,
-    it_skills TEXT,
-    education_level TEXT,
-    department TEXT,
-    role_category TEXT,
-    work_mode VARCHAR(50),
-    min_salary DECIMAL(10, 2),
-    max_salary DECIMAL(10, 2),
-    min_experience INT,
-    max_experience INT,
-    candidate_industry VARCHAR(255),
-    diversity_hiring BOOLEAN,
-    job_description TEXT,
-    no_of_vacancy INT UNSIGNED,
-    company_name VARCHAR(255),
-    company_address TEXT,
-    company_website VARCHAR(255),
-    about_company TEXT,
-    schedule_job_refresh DATETIME,
-    job_creation_date DATETIME DEFAULT CURRENT_TIMESTAMP,
-	expiry_in_days INT,
-	is_active BOOLEAN
-) AUTO_INCREMENT = 1000000000;
- 
-SELECT * FROM job_post; 
+USE candidateprofile; 
+CREATE TABLE `job_posts` (
+  `job_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `jobTitle` varchar(255) NOT NULL,
+  `employmentType` varchar(100) NOT NULL,
+  `keySkills` text NOT NULL,
+  `department` varchar(255) NOT NULL,
+  `workMode` varchar(50) NOT NULL,
+  `locations` varchar(255) NOT NULL,
+  `industry` varchar(255) DEFAULT NULL,
+  `diversityHiring` tinyint(1) DEFAULT 0,
+  `jobDescription` text NOT NULL,
+  `multipleVacancies` tinyint(1) DEFAULT 0,
+  `companyName` varchar(255) DEFAULT NULL,
+  `companyInfo` text DEFAULT NULL,
+  `companyAddress` text DEFAULT NULL,
+  `min_salary` decimal(10,2) DEFAULT NULL,
+  `max_salary` decimal(10,2) DEFAULT NULL,
+  `min_experience` int(11) DEFAULT NULL,
+  `max_experience` int(11) DEFAULT NULL,
+  `job_creation_date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `is_active` tinyint(1) DEFAULT 1,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`job_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4,
+AUTO_INCREMENT = 1000000000;
+
+ALTER TABLE job_posts ADD COLUMN recruiter_id INT NOT NULL;
+ALTER TABLE job_posts
+ADD COLUMN status ENUM('pending', 'approved', 'rejected') DEFAULT 'pending',
+ADD COLUMN approvedBy INT NULL;  -- Stores admin ID who approved it (NULL if pending)
+
+SELECT * FROM job_posts; 
+
+
+
+
+
+
